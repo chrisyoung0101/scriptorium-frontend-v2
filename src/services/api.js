c73f1e9bb65fdb8f976239921f198a681f938fb1
chrisyoung0101 <chrisyoung0101@gmail.com>
2025-02-23 08:09:31 -0600

    Cors

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,6 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: '/api',  // ✅ Uses Netlify proxy
-});
-
-export default api;
+await api.post('/documents', payload, {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  });
+  
